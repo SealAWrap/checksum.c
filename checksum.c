e2107ba0ecb7a4c3c716afe45f483744726571aa
SealAWrap <jeovanyguillen.j@gmail.com>
2021-02-16 12:51:40 -0800

    It will almost run

--- checksum.c
+++ checksum.c
@@ -10,6 +10,12 @@
 
 #include "stdio.h"
 #include "stdlib.h"
+#include <unistd.h>
+//#include <fcntl.h>
+#include <sys/types.h>
+//#include <sys/uio.h>
+//#include <unistd.h>
+
 
 #define max_int (255)
 #define byte unsigned char
@@ -18,13 +24,27 @@ int main (int argc, char * argv[], char ** envp) {
 
   int count = 10;
   int sum = 0;   
+  int quotient, remainder, retval;
   byte checksum; 
   byte complement;
   byte header[10];
 
   /* the following is the prototype for the read system call */
   /* int read(int fildes, void *buf, size_t nbyte);  */
-  int read(int fildes, void *buf, size_t nbyte); 
+  //int read(int fildes, void *buf, size_t nbyte);
+  retval = read(STDIN_FILENO, (void *) &header, 10); 
+  while (count > 0) {  
+    if (count == 5) {
+      checksum = header[count];
+    }else{
+      sum += header[count];
+    }
+    count--;
+  }
+  quotient = sum / (max_int + 1);
+  remainder = sum % (max_int + 1);
+  sum = quotient + remainder;
+  complement = max_int - sum;
   
 
   fprintf(stdout, "Stored Checksum: %d, Computed Checksum: %d\n", checksum, complement);
